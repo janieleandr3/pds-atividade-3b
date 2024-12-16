b9385f385e5aec2a5b354171f57c39906c85f5b0
Maria Janiele De Souza André <20211064010036@ifrn.local>
2024-12-16 17:43:30 -0300

    Projeto pronto!

---
+++ Atividade/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Janiele", 18);
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+    }
+}
